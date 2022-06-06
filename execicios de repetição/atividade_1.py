2a74ca1d88d4d31026fae105ceb256aa5239c889
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-06 09:41:37 -0300

    Delete execicios de repetição directory

--- execicios de repetição/atividade_1.py
+++
@@ -1,6 +0,0 @@
-# Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhados no mês. Calcule e mostre o total de seu salário no referido mês.
-
-sh = float(input('Qual seu sálario por hora? '))
-ht = float(input('Quantas horas trabalhou esse mês? '))
-sm = sh * ht
-print (f'Seu salário esse mês será {sm}.')
--- execicios de repetição/atividade_3.py
+++
@@ -1,7 +0,0 @@
-# faça um programa que peça as 4 notas bimestrais e mostre a media.
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-n3 = float(input('Digite a terceira nota: '))
-n4 = float(input('Digite a quarta nota: '))
-media = (n1 + n2 + n3 + n4) / 4
-print (f'Sua média é {media}')
