90fc88a6acd5b5f73cc01cc27a9ebb573f356a4b
Laiz Carvalho Zanlucas <104013116+zanlucaslaiz@users.noreply.github.com>
2022-06-06 09:59:17 -0300

    Add files via upload

---
+++ estruturaDeRepeticao/atividade04.py
@@ -0,0 +1,6 @@
+# faça um programa que peça 2 números inteiros e um numero real. Calcule e mostre
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+total = n1 + n2
+print (f'O resultado da soma de {n1} e {n2} é {total:.1f}.')
