86d72402a0c35c6909bd075ac7e3c3d50b0abf78
zanlucaslaiz <zanlucaslaiz@gmail.com>
2022-10-24 10:06:16 -0300

    Resolução do exercicio

--- classes/atividade_03.py
+++ classes/atividade_03.py
@@ -1,5 +1,13 @@
 # Escreva uma classe em Python que leia dois números e implemente uma exponenciação.
 
-class NumbersExponentiation:
-    def __init__(self, num):
-        num = int
+class Exponenciacao:
+    def __init__(self, num1) -> None:
+        self.num1 = num1
+    
+    def exponencial(self, num2):
+        if isinstance(num2, Exponenciacao):               
+            exp = self.num**num2.num
+        else:
+            exp = self.num**num2
+
+        return exp
