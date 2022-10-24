759cd3160384258e691a7f9ec64d6048bb454e6f
zanlucaslaiz <zanlucaslaiz@gmail.com>
2022-10-24 10:41:43 -0300

    Resposta do exercicio

--- classes/atividade_03.py
+++ classes/atividade_03.py
@@ -1,6 +1,10 @@
 # Escreva uma classe em Python que leia dois números e implemente uma exponenciação.
 
 class Exponencial:
+    def __init__(self, num1, num2) -> None:
+        self.num1 = num1 
+        self.num2 = num2
+        
     def elevacao(num1=1, num2=1):
         resposta = num1 ** num2
         return resposta
